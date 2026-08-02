8b5f20890d79e537b09fba2fee4c59ff8a37781e
Knicksin4 <95506297+Knicksin4@users.noreply.github.com>
2026-08-02 15:48:49 -0700

    Add: validate-parentheses - submission-16

---
+++ Data Structures & Algorithms/validate-parentheses/submission-16.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        ctoa = { ")": "(", "}": "{", "]": "["}
+        stack = []
+
+        for c in s:
+            if c in ctoa:
+                if not stack:
+                    return False
+                elif stack[-1] != ctoa[c]:
+                    return False
+                else:
+                    stack.pop()
+            else:
+                stack.append(c)
+        return not stack
+                
