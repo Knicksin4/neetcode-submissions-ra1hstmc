2d206fd1583410314d47a0c16f2c39cc75b27926
Knicksin4 <95506297+Knicksin4@users.noreply.github.com>
2026-08-14 16:47:58 -0700

    Add: design-twitter-feed - submission-3

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-3.py
@@ -0,0 +1,39 @@
+class Twitter:
+
+    def __init__(self):
+        self.count = 0
+        self.tweetmap = defaultdict(list)
+        self.followmap = defaultdict(set)
+        
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweetmap[userId].append([self.count, tweetId])
+        self.count -= 1
+             
+    def getNewsFeed(self, userId: int) -> List[int]:
+        self.followmap[userId].add(userId)
+        res = []
+        minheap = []
+
+        for followeeId in self.followmap[userId]:
+            if followeeId in self.tweetmap:
+                index = len(self.tweetmap[followeeId]) - 1
+                count, tweetid = self.tweetmap[followeeId][index]
+                minheap.append([count, tweetid, followeeId, index - 1])
+
+        heapq.heapify(minheap)
+        while minheap and len(res) < 10:
+            count, tweetid, followeeId, index = heapq.heappop(minheap)
+            res.append(tweetid)
+            if index >= 0:
+                count,tweetid = self.tweetmap[followeeId][index]
+                heapq.heappush(minheap, [count, tweetid, followeeId, index -1])
+        return res
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followmap[followerId].add(followeeId)
+        
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.followmap[followerId]:
+            self.followmap[followerId].remove(followeeId)
